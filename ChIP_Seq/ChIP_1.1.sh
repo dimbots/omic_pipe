78b822805f63da710f17d84d44b5434b649dfed5
Dimbo <dim.botskaris@gmail.com>
2022-05-27 10:18:44 +0300

    add

--- ChIP_Seq/ChIP_1.1.sh
+++ ChIP_Seq/ChIP_1.1.sh
@@ -39,6 +39,8 @@ mv *.T.fastq.gz *.log trimmed/
 	tput setaf 3; tput bold; echo "    Results are saved in fastq and trimmed directories"
 	tput setaf 2; tput bold; echo " "
 
+#############################################################################################################################################
+
 # Mapping
 
 ln -s trimmed/*.gz .
@@ -88,6 +90,8 @@ done
 
 rm *.T.fastq.gz
 
+#############################################################################################################################################
+
 # Normalization
 
 for bam in $(ls *.bam)
@@ -130,6 +134,8 @@ for bam in $(ls *.bam)
 			;;
 		esac
 done
+
+#############################################################################################################################################
 
 # QC metrics
 
@@ -191,8 +197,11 @@ mkdir normalized | mv *.bw normalized
 	tput setaf 3; tput bold; echo "    Results are saved in mapping and normalized and qc_directories directories"
 	tput setaf 2; tput bold; echo " "
 
+#############################################################################################################################################
+
 # Compute matrix and plot heatmaps
 
+THREADS=$(grep "THREADS:" config | awk '{print substr($0,length,1)}')
 PLOT=$(grep "PLOT" config | cut -d ":" -f2)
 
 if [ $PLOT == "FALSE" ]
@@ -213,7 +222,7 @@ ln -s normalized/*.bw .
  		tes=$(grep "TES" config | cut -d ":" -f2)
  		body=$(grep "BODY" config | cut -d ":" -f2)
  		gtf=$(grep "PATH TO GTF" config | cut -d ":" -f2)
-
+		method=$(grep "REFERENCE_POINT" config | cut -d ":" -f2)
 
 		out_matrix=$(echo $bw |cut -d "." -f1 | awk '{print $1 ".out_matrix"}')
 		out_regions=$(echo $bw |cut -d "." -f1 | awk '{print $1 ".out_regions"}')
@@ -222,42 +231,56 @@ ln -s normalized/*.bw .
 		tput setaf 3; tput bold; echo "    Compute Matrix of $bw sample"
 		tput setaf 2; tput bold; echo " "
 
-#		computeMatrix scale-regions \
-#		-S $bw \
-#		-R $gtf \
-#		--beforeRegionStartLength $tss \
-#		--regionBodyLength $body \
-#		--afterRegionStartLength $tes \
-#		--smartLabels --startLabel "TSS" \
-#		--endLabel "TES" \
-#		--skipZeros -o $out_matrix \
-#		--outFileSortedRegions $out_regions \
-#		-p $THREADS
 
-		computeMatrix reference-point \
-		--referencePoint TSS \
-		-b 4000 \
-		-a 4000 \
-		-R $gtf \
-		-S $bw \
-		--skipZeros \
-		-o $out_matrix \
-		-p $THREADS
+		if [ $method == "TRUE" ]
 
-		out_plot=$(echo $bw |cut -d "." -f1 | awk '{print $1 ".Heatmap.png"}')
+			then
+			tput setaf 3; tput bold; echo "     Generate Reference Point Heatmap"
 
-#		plotHeatmap -m $out_matrix -out $out_plot --colorMap YlGnBu --boxAroundHeatmaps no --missingDataColor 1
+			computeMatrix reference-point \
+			--referencePoint TSS \
+			-b $tss \
+			-a $tes \
+			-R $gtf \
+			-S $bw \
+			--skipZeros \
+			-o $out_matrix \
+			-p $THREADS
 
-		plotHeatmap -m $out_matrix -out $out_plot --colorMap GnBu -x " " --missingDataColor 1
+			out_plot=$(echo $bw |cut -d "." -f1 | awk '{print $1 ".Heatmap.png"}')
+			plotHeatmap -m $out_matrix -out $out_plot --colorMap GnBu -x " " --missingDataColor 1
+
+
+		else
+			tput setaf 3; tput bold; echo "     Generate Scale Regions Heatmap"
+			computeMatrix scale-regions \
+			-S $bw \
+			-R $gtf \
+			--beforeRegionStartLength $tss \
+			--regionBodyLength $body \
+			--afterRegionStartLength $tes \
+			--smartLabels --startLabel "TSS" \
+			--endLabel "TES" \
+			--skipZeros -o $out_matrix \
+			--outFileSortedRegions $out_regions \
+			-p $THREADS
+
+			out_plot=$(echo $bw |cut -d "." -f1 | awk '{print $1 ".Heatmap.png"}')
+			plotHeatmap -m $out_matrix -out $out_plot --colorMap GnBu -x " " --missingDataColor 1
+
+		fi
 	done
 
 fi
-mkdir heatmaps | mv *.png *.out_matrix *.out_regions heatmaps
+mkdir heatmaps
+mv *.png *.out_matrix *.out_regions heatmaps
 rm *.bw
 
 	tput setaf 2; tput bold; echo " "
 	tput setaf 3; tput bold; echo "    Results are saved in heatmaps directory"
 	tput setaf 2; tput bold; echo " "
+
+#############################################################################################################################################
 
 # Peak Calling
 
@@ -393,6 +416,8 @@ tput setaf 2; tput bold; echo " "
 
 esac
 
+#############################################################################################################################################
+
 # Downsampling
 
 tput setaf 3; tput bold; echo " "
@@ -482,6 +507,8 @@ tput setaf 2; tput bold; echo " "
 *)
 
 esac
+
+#############################################################################################################################################
 
 # Annotation
 
