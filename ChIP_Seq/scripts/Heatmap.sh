5bb1f0a5c6ae723e0f7ee8cbbeae66e88bfa96c1
Dimbo <dim.botskaris@gmail.com>
2022-10-31 11:48:21 +0200

    add

--- ChIP_Seq/scripts/Heatmap.sh
+++ ChIP_Seq/scripts/Heatmap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Compute matrix and plot heatmaps
+
 THREADS=$(grep "THREADS:" config | awk '{print substr($0,length,1)}')
 PLOT=$(grep "PLOT" config | cut -d ":" -f2)
 
@@ -11,7 +12,8 @@ if [ $PLOT == "FALSE" ]
 
 else
 
-ln -s normalized/*.bw .
+ln -s downsampled/*.bw .
+ln -s peak_calling/* .
 
 	for bw in $(ls *.bw)
 
@@ -37,25 +39,34 @@ ln -s normalized/*.bw .
 			then
 			tput setaf 3; tput bold; echo "     Generate Reference Point Heatmap"
 
+			tput setaf 6; tput bold; echo "Set peak file for sample $bw file."
+                        tput setaf 2; tput bold; echo " "
+                        read peak_file
+
 			computeMatrix reference-point \
-			--referencePoint TSS \
+			--referencePoint center \
 			-b $tss \
 			-a $tes \
-			-R $gtf \
+			-R $peak_file \
 			-S $bw \
 			--skipZeros \
 			-o $out_matrix \
 			-p $THREADS
 
 			out_plot=$(echo $bw |cut -d "." -f1 | awk '{print $1 ".Heatmap.png"}')
-			plotHeatmap -m $out_matrix -out $out_plot --colorMap GnBu -x " " --missingDataColor 1
+			plotHeatmap -m $out_matrix -out $out_plot --interpolationMethod nearest -z "Peaks" -x " " --boxAroundHeatmaps no  --colorList white,whitesmoke,seashell,mistyrose,salmon,r  --missingDataColor 1
 
 
 		else
 			tput setaf 3; tput bold; echo "     Generate Scale Regions Heatmap"
+
+			tput setaf 6; tput bold; echo "Set peak file for sample $bw file."
+                        tput setaf 2; tput bold; echo " "
+                        read peak_file
+
 			computeMatrix scale-regions \
 			-S $bw \
-			-R $gtf \
+			-R $peak_file \
 			--beforeRegionStartLength $tss \
 			--regionBodyLength $body \
 			--afterRegionStartLength $tes \
@@ -66,7 +77,7 @@ ln -s normalized/*.bw .
 			-p $THREADS
 
 			out_plot=$(echo $bw |cut -d "." -f1 | awk '{print $1 ".Heatmap.png"}')
-			plotHeatmap -m $out_matrix -out $out_plot --colorMap GnBu -x " " --missingDataColor 1
+			plotHeatmap -m $out_matrix -out $out_plot --interpolationMethod nearest -z "Peaks" -x " " --boxAroundHeatmaps no  --colorList white,whitesmoke,seashell,mistyrose,salmon,r --missingDataColor 1
 
 		fi
 	done
@@ -74,7 +85,8 @@ ln -s normalized/*.bw .
 fi
 mkdir heatmaps
 mv *.png *.out_matrix *.out_regions heatmaps
-rm *.bw
+
+find -type l -delete
 
 	tput setaf 2; tput bold; echo " "
 	tput setaf 3; tput bold; echo "    Results are saved in heatmaps directory"
--- ChIP_Seq/scripts/Peak_Calling.sh
+++ ChIP_Seq/scripts/Peak_Calling.sh
@@ -1,155 +1,145 @@
 #!/bin/bash
 
-# Peak Calling
-
-tput setaf 2; tput bold; echo " "
-tput setaf 3; tput bold; echo "    Peak Calling with MACS2"
-tput setaf 2; tput bold; echo " "
-
-
-peak_c=$(grep "PEAK_CALLING" config | cut -d ":" -f2)
+peak_c=$(grep "PEAK_CALLING_MACS" config | cut -d ":" -f2)
 
 case $peak_c in
 
 TRUE)
 
-ln -s mapping/*.bam .
-ln -s mapping/*.bam.bai .
-
-while [[ $treatment != "n" ]]
-
-	do
-
-	tput setaf 6; tput bold; echo "Type treatment bam file. else type (n)"
 	tput setaf 2; tput bold; echo " "
-	read treatment
+	tput setaf 3; tput bold; echo "    Peak Calling Analysis with MACS2"
+	tput setaf 2; tput bold; echo " "
 
-		if [[ $treatment = "n" ]]
+ln -s downsampled/*.bam .
+ln -s downsampled/*.bam.bai .
+
+	while [[ $treatment != "EXIT" ]]
+
+        do
+
+        tput setaf 6; tput bold; echo "Set treatment bam file. To abord peak call analysis type (EXIT)"
+        tput setaf 2; tput bold; echo " "
+        read treatment
+
+		if [[ $treatment = "EXIT" ]]
 
 			then
-			break
+                       	break
 
 			else
+			tput setaf 6; tput bold; echo "Set input bam file. If there is not input file type (no_input) "
+                        tput setaf 2; tput bold; echo " "
+                        read input
 
-			tput setaf 6; tput bold; echo "Type input bam file. If input file does not exist type (n)"
-			tput setaf 2; tput bold; echo " "
-			read input
-			tput setaf 6; tput bold; echo "Type out file. e.g peaks_sample - else type (n)"
-			tput setaf 2; tput bold; echo " "
-			read out
+			tput setaf 6; tput bold; echo "Set output file, e.g sample name"
+                        tput setaf 2; tput bold; echo " "
+                        read out
 
-			broad=$(grep "BROAD" config | cut -d ":" -f2)
 			g=$(grep "GENOME_SP" config | cut -d ":" -f2)
-
-			case $broad in
-
-				TRUE)
-
-				if [[ $input == "n" ]]
-
-					then
-					#broad no input
-					macs2 callpeak --treatment $treatment --nomodel --broad --format BAM --gsize $g -n $out
-
-					else
-					#broad input
-					macs2 callpeak --treatment $treatment --control $input --nomodel --broad --format BAM --gsize $g -n $out
-
-				fi
-
-				;;
-
-				FALSE)
-
-				if [[ $input == "n" ]]
-
-					then
-					#default no input
-					macs2 callpeak --treatment $treatment --nomodel --format BAM --gsize $g -n $out
-
-					else
-					#default input
-					macs2 callpeak --treatment $treatment --control $input --nomodel --format BAM --gsize $g -n $out
-				fi
-
-				;;
-
-				*)
-
-				;;
-
-			esac
-		fi
-
-done
-
-# Merge peaks from replicates
-
-while [[ $repA != "n" ]]
-
-	do
-
-	tput setaf 3; tput bold; echo " "
-	tput setaf 3; tput bold; echo "    Merge Peaks From Replicates"
-	tput setaf 3; tput bold; echo " "
-
-	tput setaf 3; tput bold; echo " "
-	tput setaf 6; tput bold; echo "type replicate A peak file. else type (n)"
-	tput setaf 2; tput bold; echo " "
-	read repA
-
-		if [[ $repA = "n" ]]
-
-			then
-			break
-
-			else
-
-			tput setaf 3; tput bold; echo " "
-			tput setaf 6; tput bold; echo "type replicate B peak file. else type (n)"
-			tput setaf 2; tput bold; echo " "
-			read repB
-
-			tput setaf 3; tput bold; echo " "
-			tput setaf 6; tput bold; echo "type peaks out file. e.g (Peaks_Merged_samples.tsv) - else type (n)"
-			tput setaf 2; tput bold; echo " "
-			read out_file
-
-#			tput setaf 3; tput bold; echo " "
-#			tput setaf 6; tput bold; echo "set path to blacklist_regions. else type (n)"
-#			tput setaf 2; tput bold; echo " "
-
-			bedtools intersect -a $repA -b $repB -wa > $out_file
+			broad=$(grep "BROAD" config | cut -d ":" -f2)
 
 
-			#final_peaks="$out_file.bed"
+			                        case $broad in
 
-#			tput setaf 3; tput bold; echo " "
-#			tput setaf 6; tput bold; echo "set path to blacklist_regions. else type (n)"
-#			tput setaf 2; tput bold; echo " "
-#			read blacklist
+		                                TRUE)
+						if [[ $input != "no_input" ]]
 
-#			bedtools intersect -v -a $out_file -b $blacklist > $final_peaks
+							then
+                	                		# broad (with input)
+                        		        	macs2 callpeak --treatment $treatment --control $input --nomodel --broad --format BAM --gsize $g -n $out
+							# Filtering (pvalue cutoff = 1.00e-06 && FDR threshold of 0.1%)
+							grep -v "#" "${out}_peaks.xls" | sed -e '1,2d' | awk '$7 > 6.34 {print $0}' | awk '$9 > 6.34 {print $0}' > "${out}_peaks.filtered.xls"
+							else
+							macs2 callpeak --treatment $treatment --nomodel --broad --format BAM --gsize $g -n $out
+						fi
 
-		fi
+		                                ;;
+
+                		                FALSE)
+						if [[ $input != "no_input" ]]
+
+                                                        then
+	      	                        		# narrow (with input)
+			                                macs2 callpeak --treatment $treatment --control $input --nomodel --format BAM --gsize $g -n $out
+							# Filtering (pvalue cutoff = 1.00e-06 && FDR threshold of 0.1%)
+                                                        grep -v "#" "${out}_peaks.xls" | sed -e '1,2d' | awk '$7 > 6.34 {print $0}' | awk '$9 > 6.34 {print $0}' > "${out}_peaks.filtered.xls"
+							else
+							macs2 callpeak --treatment $treatment --nomodel --format BAM --gsize $g -n $out
+						fi
+
+                		                ;;
+
+        	                	        *)
+
+                        	       		 ;;
+
+			                        esac
+                fi
+
 done
 
 mkdir peak_calling
-mv *.xls *.gappedPeak *.broadPeak peak_calling
-mkdir peak_calling/merged
-mv *.tsv peak_calling/merged
-rm *.bam *.bai
+# narrow
+mv *.narrowPeak peak_calling
+mv *.xls peak_calling
+# broad
+mv *.broadPeak peak_calling
+mv *.gappedPeak peak_calling
+
 
 ;;
 
 FALSE)
 
-tput setaf 3; tput bold; echo " "
-tput setaf 6; tput bold; echo "    Peak Calling Aborted"
 tput setaf 2; tput bold; echo " "
+tput setaf 3; tput bold; echo "    Peak Calling Analysis with SICER"
+tput setaf 2; tput bold; echo " "
+
+ln -s downsampled/*.bam .
+ln -s downsampled/*.bam.bai .
+mkdir peak_calling
+
+	while [[ $treatment != "EXIT" ]]
+
+        do
+
+        tput setaf 6; tput bold; echo "Set treatment bam file. To abord peak call analysis type (EXIT)"
+        tput setaf 2; tput bold; echo " "
+        read treatment
+
+		if [[ $treatment = "EXIT" ]]
+
+			then
+                       	break
+
+			else
+			tput setaf 6; tput bold; echo "Set input bam file. If there is not input file type (no_input) "
+                        tput setaf 2; tput bold; echo " "
+                        read input
+
+			tput setaf 6; tput bold; echo "Set output file, e.g sample name"
+                        tput setaf 2; tput bold; echo " "
+                        read out
+
+				if [[ $input != "no_input" ]]
+
+					then
+                	                # (with input)
+					sicer -t $treatment -c $input -s mm10 -w 200 -g 600 --false_discovery_rate 0.001 --effective_genome_fraction 0.88 -o $out -cpu 7
+					mv $out peak_calling
+					else
+					sicer -t $treatment -s mm10 -w 200 -g 600 --false_discovery_rate 0.001 --effective_genome_fraction 0.88 -o $out -cpu 7
+					mv $out peak_calling
+				fi
+
+                fi
+
+done
 
 ;;
 
 *)
 
 esac
+rm *.bam
+rm *.bai
